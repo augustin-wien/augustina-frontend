de8b0dc5f33ea74d9d2eecdeb7b85288cf51682b
Riddy999 <felix.effenberg@live.com>
2023-05-15 11:01:57 +0200

    new color options

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/color">Color Settings</RouterLink>
+        <RouterLink to="/color">Color Menu</RouterLink>
       </nav>
     </div>
   </header>
