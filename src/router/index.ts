74638c78e878ce67ba2bdc0ba0df82ec4623359b
Riddy999 <felix.effenberg@live.com>
2023-05-15 11:08:36 +0200

    npm install + new component

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,6 +16,12 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/color',
+      name: 'color',
+      
+      component: () => import('../views/ColorPicker.vue')
     }
   ]
 })
