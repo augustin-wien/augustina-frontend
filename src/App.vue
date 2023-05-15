c001dce3467a7cb40c4f9f38f52d37959e466457
Riddy999 <felix.effenberg@live.com>
2023-05-15 14:22:52 +0200

    install: tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/color">Color Menu</RouterLink>
+        <RouterLink to="/landingpage">Landing</RouterLink>
       </nav>
     </div>
   </header>
