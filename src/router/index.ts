20e09f41804b45ad0aab942d0d203240dd4ebd97
12123434 <e12123434@student.tuwien.ac.at>
2023-05-27 19:29:00 +0200

    fix: removed unnecessary files

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,27 +6,7 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/color',
-      name: 'color',
-      
-      component: () => import('../views/ColorPicker.vue')
-    },
-    {
-      path: '/landingpage',
-      name: 'landingpage',
-      
+      name: 'Landingpage',
       component: () => import('../views/LandingPage.vue')
     }
   ]
