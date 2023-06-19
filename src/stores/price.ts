0a3dd2ac67c2b0b3ba958ad28bb59d0cd0e57863
Riddy999 <felix.effenberg@live.com>
2023-06-19 12:15:20 +0200

    implementation: Tipping Page

--- src/stores/price.ts
+++ src/stores/price.ts
@@ -1,23 +1,38 @@
+import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import { storeToRefs } from 'pinia'
 
 export const usePriceStore = defineStore('price', {
   
     state: () =>{
       return{
-        value: 3
+        euro: 3,
+        cent: 0
       }
     },
     
     actions: {
       increment() {
-        this.value++
+        this.euro++
+      },
+
+      // increment with custom values for euro (e) and cent (c)
+      incrementec(e: number, c: number) {
+        this.euro = this.euro + e
+        this.cent = this.cent + c
       },
   
       decrement() {
-        if(this.value > 3){
-            this.value--
+        if(this.euro > 3){
+            this.euro--
         }
       },
+
+      // decrement with custom values for euro (e) and cent (c)
+      decrementec(e: number, c: number) {
+        this.euro = this.euro - e
+        this.cent = this.cent - c
+      }
     }
   }
   )
---
+++ src/views/TippingPage.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+    import { usePriceStore } from '@/stores/price'
+    import { RouterLink} from 'vue-router'
+    const priceStore = usePriceStore()
+    const increment = priceStore.increment
+    const incrementec = priceStore.incrementec
+</script>
+
+<template>
+  <main  className="h-screen px-10 py-16 grid grid-rows-[7] place-items-center">
+    <header >
+        <img alt="Augustin logo" className="logo mx-auto my-5" src="@/assets/logo.svg" width="270" height="150"/>
+    </header>
+    <div className="text-center font-semibold text-4xl">
+        Trinkgeld geben
+    </div>
+    <div className="flex place-items-center w-full">
+        <button @click="incrementec(0, 25)" class="bg-green-600 rounded-full p-7 text-white text-4xl font font-semibold w-full">
+            <RouterLink to="/">0,25 €</RouterLink>
+        </button>
+    </div>
+    <div className="flex place-items-center w-full">
+        <button @click="incrementec(0, 50)" class="bg-green-600 rounded-full p-7 text-white text-4xl font font-semibold w-full">
+            <RouterLink to="/">0,50 €</RouterLink>
+        </button>
+    </div>
+    <div className="flex place-items-center w-full">
+        <button @click="increment" class="bg-green-600 rounded-full p-7 text-white text-4xl font font-semibold w-full">
+            <RouterLink to="/">1,00 €</RouterLink>
+        </button>
+    </div>
+    <div className="flex place-items-center w-full">
+        <button class="bg-green-600 rounded-full p-7 text-white text-4xl font font-semibold w-full">
+            <RouterLink to="/">Eigener Betrag</RouterLink>
+        </button>
+    </div>
+    <div className="flex place-items-center w-full">
+        <button class="bg-green-600 rounded-full p-7 text-white text-4xl font font-semibold w-full">
+            <RouterLink to="/">Heute nicht</RouterLink>
+        </button>
+    </div>
+  </main>
+</template>
