6c336907be814549dd4ce47d9106b2a672e08088
Riddy999 <felix.effenberg@live.com>
2023-07-10 14:24:17 +0200

    Fix: Button updated

--- src/views/PrintDigital.vue
+++ src/views/PrintDigital.vue
@@ -14,14 +14,14 @@
 <template>
   <main  className="h-screen px-10 py-16 grid grid-rows-6 place-items-center">
     <header >
-        <img alt="Augustin logo" className="logo mx-auto my-5" src="@/assets/logo.svg" width="270" height="150"/>
+        <img alt="Augustin logo" className="logo mx-auto my-2" src="@/assets/logo.svg" width="270" height="150"/>
     </header>
     <div className="text-center font-semibold text-4xl">
         Ausgabe wählen
     </div>
     <div className="flex place-items-center w-full">
         <button @click="digital" class="bg-green-600 rounded-full p-7 text-white text-4xl font font-semibold w-full">
-            <RouterLink to="/additionalproducts">ePaper</RouterLink>
+            <RouterLink to="/additionalproducts">E-Paper</RouterLink>
         </button>
     </div>
     <div className="flex place-items-center w-full">
