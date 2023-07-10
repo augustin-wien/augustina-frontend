0b82867b77fc6a596863cf1951590ab1915b5bd2
Riddy999 <felix.effenberg@live.com>
2023-07-10 13:49:34 +0200

    fix: improved Layout

--- src/views/InformationPrintEpaper.vue
+++ src/views/InformationPrintEpaper.vue
@@ -3,20 +3,20 @@
 </script>
 
 <template>
-  <main  className="h-screen px-10 py-16 grid grid-rows-6 place-items-center">
-    <header >
-        <img alt="Augustin logo" className="logo mx-auto my-5" src="@/assets/logo.svg" width="270" height="150"/>
-    </header>
-    <div className="text-center font-semibold text-4xl">
-        Epaper
+  <main  className="h-screen px-10 py-16 grid grid-rows-">
+    <div className="place-items-center">
+        <img alt="Augustin logo" className="logo mx-auto my-2" src="@/assets/logo.svg" width="270" height="150"/>
     </div>
-    <div className="flex place-items-center w-full row-span-3   ">
-        E-Paper ist einen digitale Zeitung, diese wird dir nach deinem Kauf in unserem online Store angezeigt
+    <div className="flex items-center font-semibold text-4xl place-content-center">
+        E-Paper
     </div>
-    <div>
+    <div className="flex text-center w-full place-content-center text-xl">
+        E-Paper ist einen digitale Zeitung, diese wird dir nach deinem Kauf in unserem online Store angezeigt.
+    </div>
+    <div className="flex items-center font-semibold text-4xl place-content-center">
         Print-Version
     </div>
-    <div>
+    <div className="flex text-center w-full place-content-center text-xl">
         Die Print-Version ist die klassische physische Zeitung. Ihr_e Verkäufer_in wird Ihnen diese nach Abschluss des Kaufes aushändigen.
     </div>
     <div className="flex place-items-center w-full">
