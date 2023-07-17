0c7dd47125a2bdb6a5d5b8c94f349962bccc8a39
Riddy999 <felix.effenberg@live.com>
2023-07-17 14:37:29 +0200

    fix: TippingPage fixed, removed redundant imports

--- src/views/PaymentStripe.vue
+++ src/views/PaymentStripe.vue
@@ -1,41 +1,18 @@
 <script setup lang="ts">
 import { settingsStore } from '@/stores/settingsStore';
-import { RouterLink } from 'vue-router'
 import { onMounted } from 'vue'
-import { useStripeStore } from '@/stores/stripe'
-
-/*
-const plugin = document.createElement("script");
-plugin.setAttribute(
-  "src",
-  "https://js.stripe.com/v3"
-);
-plugin.async = true;
-document.head.appendChild(plugin);
-*/
-
-/*
-const cardel = element.create()
-loadStripe('tc2yNFPJ9DYSmhNnf04vCUmbM3f MS94WK2w1YuhiTcxMIti8p3etufbrsr1oJpG2OUaLUmNUTU00cxAmOXLZ').then((val:any) =>{
-  stripeStore.stripe = val 
-  const elements = val.elements()
-  const card = elements.create('card', {style: style})
-}
-)
-*/
-
-
 
 const settStore = settingsStore()
 onMounted(() => {
   settStore.fetchSettings()
   //@ts-ignore
+  //Will become env variable
   const stripe = Stripe('pk_test_51NPjKHElvUd6Cd58X0mtD30StUI2ugja89BsBuFmmBFAX4vswuAIy0w56rgd6VQwVz2aC42AQm3SvSeSuuZFqUug00QL7BuzzK')
 
   const options = {
     mode: 'payment',
     amount: 1099,
-    currency: 'usd',
+    currency: 'eur',
     // Fully customizable with appearance API.
     appearance: {/*...*/ },
   };
@@ -51,13 +28,24 @@ onMounted(() => {
 </script> 
 
 <template>
-  <form id="payment-form">
-    <div id="payment-element">
-      <!-- Elements will create form elements here -->
+  <main className="h-screen px-10 py-16 grid grid-rows-6 place-items-center">
+    <header >
+        <img alt="Augustin logo" className="logo mx-auto my-5" src="@/assets/logo.svg" width="270" height="150"/>
+    </header>
+    <div className="row-span-2">
+      Hier werden die Produkte Abgebildet werden
     </div>
-    <button id="submit">Submit</button>
-    <div id="error-message">
-      <!-- Display error message to your customers here -->
-    </div>
-  </form>
+    <form id="payment-form" className="row-span-3">
+      <div id="payment-element">
+        <!-- Elements will create form elements here -->
+      </div>
+      <div className="grid grid-cols-2 grid-rows-1 items-center">
+        <button id="submit" className="bg-red-600 hover:bg-red-700 text-white font-bold my-3 mr-4 py-2 px-2 rounded-full">Abbrechen</button>
+        <button id="submit" className="bg-green-600 hover:bg-green-700 text-white font-bold my-3 ml-4 py-2 px- rounded-full">Bestätigen</button>
+      </div>
+      <div id="error-message">
+        <!-- Display error message to your customers here -->
+      </div>
+    </form>
+  </main>
 </template>
--- src/views/TippingPage.vue
+++ src/views/TippingPage.vue
@@ -7,17 +7,12 @@
 </script>
 
 <template>
-  <main  className="h-screen px-10 py-16 grid grid-rows-[7] place-items-center">
+  <main  className="h-screen px-10 py-16 grid grid-rows-6 place-items-center">
     <header >
         <img alt="Augustin logo" className="logo mx-auto my-5" src="@/assets/logo.svg" width="270" height="150"/>
     </header>
     <div className="text-center font-semibold text-4xl">
         Trinkgeld geben
-    </div>
-    <div className="flex place-items-center w-full">
-        <button @click="incrementec(0.25)" class="bg-green-600 rounded-full p-7 text-white text-4xl font font-semibold w-full">
-            <RouterLink to="/confirmation">0,25 €</RouterLink>
-        </button>
     </div>
     <div className="flex place-items-center w-full">
         <button @click="incrementec(0.50)" class="bg-green-600 rounded-full p-7 text-white text-4xl font font-semibold w-full">
