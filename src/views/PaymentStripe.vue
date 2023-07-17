5ae20b4651f4fff667340efe73c5f626651ced4a
Riddy999 <felix.effenberg@live.com>
2023-07-17 14:10:26 +0200

    fix: Lifehook cycle fixed

--- src/views/PaymentStripe.vue
+++ src/views/PaymentStripe.vue
@@ -1,48 +1,53 @@
-<script lang="ts">
-    import { settingsStore } from '@/stores/settingsStore';
-    import { RouterLink} from 'vue-router'
-    import { onMounted } from 'vue'
-    import { useStripeStore } from '@/stores/stripe'
+<script setup lang="ts">
+import { settingsStore } from '@/stores/settingsStore';
+import { RouterLink } from 'vue-router'
+import { onMounted } from 'vue'
+import { useStripeStore } from '@/stores/stripe'
 
-    /*
-    const plugin = document.createElement("script");
-    plugin.setAttribute(
-      "src",
-      "https://js.stripe.com/v3"
-    );
-    plugin.async = true;
-    document.head.appendChild(plugin);
-    */
-    //@ts-ignore
-    const stripe = Stripe('pk_test_51NPjKHElvUd6Cd58X0mtD30StUI2ugja89BsBuFmmBFAX4vswuAIy0w56rgd6VQwVz2aC42AQm3SvSeSuuZFqUug00QL7BuzzK')
+/*
+const plugin = document.createElement("script");
+plugin.setAttribute(
+  "src",
+  "https://js.stripe.com/v3"
+);
+plugin.async = true;
+document.head.appendChild(plugin);
+*/
 
-    const options = {
+/*
+const cardel = element.create()
+loadStripe('tc2yNFPJ9DYSmhNnf04vCUmbM3f MS94WK2w1YuhiTcxMIti8p3etufbrsr1oJpG2OUaLUmNUTU00cxAmOXLZ').then((val:any) =>{
+  stripeStore.stripe = val 
+  const elements = val.elements()
+  const card = elements.create('card', {style: style})
+}
+)
+*/
+
+
+
+const settStore = settingsStore()
+onMounted(() => {
+  settStore.fetchSettings()
+  //@ts-ignore
+  const stripe = Stripe('pk_test_51NPjKHElvUd6Cd58X0mtD30StUI2ugja89BsBuFmmBFAX4vswuAIy0w56rgd6VQwVz2aC42AQm3SvSeSuuZFqUug00QL7BuzzK')
+
+  const options = {
     mode: 'payment',
     amount: 1099,
     currency: 'usd',
     // Fully customizable with appearance API.
-    appearance: {/*...*/},
-    };
+    appearance: {/*...*/ },
+  };
 
-    // Set up Stripe.js and Elements to use in checkout form
-    const elements = stripe.elements(options);
+  // Set up Stripe.js and Elements to use in checkout form
+  const elements = stripe.elements(options);
 
-    // Create and mount the Payment Element
-    const paymentElement = elements.create('payment');
-    paymentElement.mount('#payment-element');
+  // Create and mount the Payment Element
+  const paymentElement = elements.create('payment');
+  paymentElement.mount('#payment-element');
 
-    /*
-    const cardel = element.create()
-    loadStripe('tc2yNFPJ9DYSmhNnf04vCUmbM3f MS94WK2w1YuhiTcxMIti8p3etufbrsr1oJpG2OUaLUmNUTU00cxAmOXLZ').then((val:any) =>{
-      stripeStore.stripe = val 
-      const elements = val.elements()
-      const card = elements.create('card', {style: style})
-    }
-    )
-    */
-
-    const settStore = settingsStore()
-    onMounted(() => {settStore.fetchSettings()})
+})
 </script> 
 
 <template>
