908cf30d426cbe320a969df528b854d2843d4b6d
Riddy999 <felix.effenberg@live.com>
2023-07-17 15:40:30 +0200

    fix: removed redundant function

--- src/api/agent.ts
+++ src/api/agent.ts
@@ -10,12 +10,6 @@ const SettingsConfiguration = {
     current: (): Promise<Settings> => axios.get('http://localhost:3000/api/settings/').then(sleep(1000)).then(responseBody)
 }
 
-const StripeLoad = {
-    //public Key will become an env variable
-    load: (): Promise<Stripe | null> => loadStripe("pk_test_51NTszUJEpEVZWKtc2yNFPJ9DYSmhNnf04vCUmbM3fMS94WK2w1YuhiTcxMIti8p3etufbrsr1oJpG2OUaLUmNUTU00cxAmOXLZ")
-}
-
 export default{
     SettingsConfiguration,
-    StripeLoad
 }
