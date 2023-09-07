a31e45e4512fc8cd27a10e71440243dbc82dcb9c
Aaron <aaron@nanu-c.org>
2023-09-07 15:44:23 +0200

    Dockerize the frontend

--- src/keycloak/keycloak.ts
+++ src/keycloak/keycloak.ts
@@ -1,7 +1,7 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-    url: 'http://localhost:8080/',
+    url: import.meta.env.VITE_KEYCLOAK_URL as string,
     realm: 'augustin',
     clientId: 'frontend'
 });
