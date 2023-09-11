0c7c73f49973279a3507929634ef2ea1de572e88
Aaron <aaron@nanu-c.org>
2023-09-11 12:50:11 +0200

    fix keycloak paths

--- src/views/404View.vue
+++ src/views/404View.vue
@@ -1,6 +1,6 @@
 <template>
     error 404
-    <div class="btn" @click="keycloak.login()">Login</div>
+    <div class="btn" @click="keycloak.keycloak.login()">Login</div>
 </template>
 
 <script setup lang="ts">
