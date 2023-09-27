2968d82fa6b55d245f7b416c1c765f5897d090a2
Aaron <aaron@nanu-c.org>
2023-09-27 23:50:07 +0200

    refresh keycloak on expired token

--- src/keycloak/keycloak.ts
+++ src/keycloak/keycloak.ts
@@ -2,12 +2,11 @@ import Keycloak from "keycloak-js";
 
 const keycloak = {
     initailizedKeycloak: false,
-    keycloak:new Keycloak({
+    keycloak: new Keycloak({
         url: import.meta.env.VITE_KEYCLOAK_URL as string,
         realm: 'augustin',
         clientId: 'frontend'
     })
 };
-
 
 export default keycloak;
