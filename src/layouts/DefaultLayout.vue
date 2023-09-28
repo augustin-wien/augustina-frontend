5c76d3679e3bde32102cd9f1489f51d91a2b245f
Aaron <aaron@nanu-c.org>
2023-09-28 22:32:43 +0200

    load logo from settings

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -2,14 +2,15 @@
     <div class="base-layout">
         <main class="absolute inset-0 px-8 pt-4 pb-16 place-items-center base-layout">
             <header class="place-items-center h-1/6">
-            <slot name="header">
-                <img alt="Augustin logo" className="logo mx-auto my-1" src="@/assets/logo.svg" width="213" height="105" />
+                <slot name="header">
+                    <img alt="Augustin logo" className="logo mx-auto my-1" :src="apiUrl + 'img/logo.png'" width="213"
+                        height="105" />
 
                 </slot>
             </header>
             <div class="h-5/6 main-container">
                 <slot name="main">
-                <h1>Default Content</h1>
+                    <h1>Default Content</h1>
                 </slot>
             </div>
         </main>
@@ -18,12 +19,27 @@
         </footer>
     </div>
 </template>
-
-<style lang="scss">
+<script setup lang="ts">
+const apiUrl = import.meta.env.VITE_API_URL
+</script>
+<style lang="scss" scoped>
 .main-container {
     display: flex;
     flex-direction: column;
     align-items: center;
     margin-top: 10px;
+}
+
+.logo {
+    max-height: 80px;
+    width: auto;
+}
+</style>
+<style>
+main {
+    max-width: 400px;
+    left: 0;
+    right: 0;
+    margin: auto;
 }
 </style>
