e787fedcdff94cf43c17cbb13a8ab5f4a06ec7f9
Riddy999 <felix.effenberg@live.com>
2023-10-02 00:35:12 +0200

    implementation: i18n translation

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,51 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
 
 import App from './App.vue'
 import router from './router'
 
+const i18n = createI18n({
+    locale: "de",
+    messages: {
+        de: {
+            buyItem: "Zeitung kaufen bei:",
+            newspaper: "Print Zeitung",
+            next: "Weiter",
+
+            donation: "Spende eingeben:",
+            edit: "Eingabe",
+            notToday: "Heute nicht",
+
+            confirm: "Bezahlung bestätigen:",
+            agreement: "Mit kauf akzeptiere ich die ",
+            terms: "AGBs",
+
+            symbol: "Symbol zeigen"
+        },
+        en: {
+            buyItem: "Buy a newspaper from:",
+            newspaper: "Newspaper",
+            next: "Next",
+
+            donation: "Give a donation:",
+            edit: "Edit",
+            notToday: "Not today",
+
+            confirm: "Confirm your order:",
+            agreement: "With this purchase, I confirm the ",
+            terms: "terms and conditions",
+
+            symbol: "Show the symbol"
+        }
+    }
+})
 
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
+app.use(i18n)
 
 app.mount('#app')
 
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -9,7 +9,7 @@ const paymentStore = usePaymentStore()
 const settStore = settingsStore()
 const fetch = settStore.getSettingsFromApi
 
-onMounted(()=>fetch())
+onMounted(() => fetch())
 </script>
 
 <template>
@@ -18,7 +18,7 @@ onMounted(()=>fetch())
             <div className="grid grid-rows-5 h-full place-items-center w-full">
                 <div class="row-span-2 grid grid-rows-3 h-full w-full">
                     <div className="text-center font-semibold text-2xl pt-5">
-                        Zeitung kaufen bei
+                        {{ $t("buyItem") }}
                     </div>
                     <div class="flex place-content-center">
                         <div class="text-center min-w-fit h-4/5 text-4xl rounded-full text-black font-bold">
@@ -27,7 +27,7 @@ onMounted(()=>fetch())
                     </div>
                     <div class="place-items-center w-full flex">
                         <div class="text-2xl grow h-[56px] text-center font-semibold text-white bg-black p-3 rounded-full">
-                            1x Print Zeitung
+                            1x {{ $t("newspaper") }}
                         </div>
                     </div>
                 </div>
@@ -35,9 +35,14 @@ onMounted(()=>fetch())
                     <p className="text-center text-8xl font-semibold">{{ paymentStore.priceInEuros() }}€</p>
                 </div>
                 <div className="place-items-center w-full flex">
+                    <select v-model="$i18n.locale">
+                        <option value="en">en</option>
+                        <option value="de">de</option>
+                    </select>
                     <RouterLink class="text-center h-[76px] grow" :to="{ name: 'Tippingpage' }">
-                        <button class="customcolor background-color rounded-full p-5 text-white text-3xl w-full font-semibold">
-                            Weiter
+                        <button
+                            class="customcolor background-color rounded-full p-5 text-white text-3xl w-full font-semibold">
+                            {{ $t("next") }}
                         </button>
                     </RouterLink>
                 </div>
@@ -47,7 +52,7 @@ onMounted(()=>fetch())
 </template>
 
 <style>
-.customcolor{
+.customcolor {
     background-color: v-bind(settStore.settings.Color);
 }
 </style>
