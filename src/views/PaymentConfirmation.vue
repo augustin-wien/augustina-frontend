e8201ef0c09b70137642fab32efb9e5a9a421a26
Riddy999 <felix.effenberg@live.com>
2023-10-03 16:03:54 +0200

    fix: checkmark size

--- src/views/PaymentConfirmation.vue
+++ src/views/PaymentConfirmation.vue
@@ -7,8 +7,8 @@
           <div>{{ paymentStore.firstName }}</div>
         </div>
         <div class="row-span-2 text-6xl font-bold w-fit h-full relative">
-          <div class="rounded-full absolute h-16 w-16 fill-white bg-green-600 right-0 top-0 place-items-center grid">
-            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="45" height="45" viewBox="0 0 30 30">
+          <div class="rounded-full absolute h-12 w-12 fill-white bg-green-600 right-0 top-0 place-items-center grid">
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 30 30">
               <path
                 d="M 26.980469 5.9902344 A 1.0001 1.0001 0 0 0 26.292969 6.2929688 L 11 21.585938 L 4.7070312 15.292969 A 1.0001 1.0001 0 1 0 3.2929688 16.707031 L 10.292969 23.707031 A 1.0001 1.0001 0 0 0 11.707031 23.707031 L 27.707031 7.7070312 A 1.0001 1.0001 0 0 0 26.980469 5.9902344 z">
               </path>
