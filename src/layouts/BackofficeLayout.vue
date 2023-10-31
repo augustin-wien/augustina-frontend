3122b42a03d840a6e18508dcb3fb81fb824cdabe
Aaron <aaron@nanu-c.org>
2023-10-31 13:58:59 +0100

    Add loading animation to backoffice

--- src/layouts/BackofficeLayout.vue
+++ src/layouts/BackofficeLayout.vue
@@ -1,30 +1,39 @@
 <template>
   <div>
-    <div className="backoffice-layout h-screen" v-if="keycloakStore.authenticated">
+    <div className="backoffice-layout h-screen" v-if="authenticated">
       <div className="">
         <div
-          className="sidemenu fixed t-0 l-0 h-full w-100 flex flex-col justify-start items-start pr-5 pl-4 border-gray-600 border-b space-y-3 pb-5 bg-lime-600">
+          className="sidemenu fixed t-0 l-0 h-full w-100 flex flex-col justify-start items-start pr-5 pl-4 border-gray-600 border-b space-y-3 pb-5 bg-lime-600"
+        >
           <div className="sidemenu-item object-center">
-            <img :src="apiUrl + 'img/logo.png'" alt="Augustin logo" className="logo mx-auto my-5" width="270"
-              height="150" />
+            <img
+              :src="apiUrl + 'img/logo.png'"
+              alt="Augustin logo"
+              className="logo mx-auto my-5"
+              width="270"
+              height="150"
+            />
           </div>
           <RouterLink to="/backoffice/vendorsummary">
             <button
-              class="flex justify-start w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded">
+              class="flex justify-start w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded"
+            >
               <font-awesome-icon :icon="faUserGroup" />
               <p class="text-base leading-4">VerkäuferInnen</p>
             </button>
           </RouterLink>
           <RouterLink to="/backoffice/credits" className="sidemenu-link">
             <button
-              class="flex justifyy-start items-center w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded">
+              class="flex justifyy-start items-center w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded"
+            >
               <font-awesome-icon :icon="faBagShopping" />
               <p class="text-base leading-4">Guthaben</p>
             </button>
           </RouterLink>
           <RouterLink to="/backoffice/payouts" className="sidemenu-link">
             <button
-              class="flex justify-start items-center w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded">
+              class="flex justify-start items-center w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded"
+            >
               <font-awesome-icon :icon="faFileLines" />
 
               <p class="text-base leading-4">Auszahlungen</p>
@@ -32,7 +41,8 @@
           </RouterLink>
           <RouterLink to="/backoffice/sales" className="sidemenu-link">
             <button
-              class="flex justify-start items-center w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded">
+              class="flex justify-start items-center w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded"
+            >
               <font-awesome-icon :icon="faDungeon" />
 
               <p class="text-base leading-4">Verkäufe</p>
@@ -40,7 +50,8 @@
           </RouterLink>
           <RouterLink to="/backoffice/payments" className="sidemenu-link">
             <button
-              class="flex justify-start items-center w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded">
+              class="flex justify-start items-center w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded"
+            >
               <font-awesome-icon :icon="faBagShopping" />
 
               <p class="text-base leading-4">Buchhaltung</p>
@@ -48,7 +59,8 @@
           </RouterLink>
           <RouterLink to="/backoffice/productsettings" className="sidemenu-link">
             <button
-              class="flex justify-start items-center w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded">
+              class="flex justify-start items-center w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded"
+            >
               <font-awesome-icon :icon="faSplotch" />
 
               <p class="text-base leading-4">Produkte</p>
@@ -56,7 +68,8 @@
           </RouterLink>
           <RouterLink to="/backoffice/settings" className="sidemenu-link">
             <button
-              class="flex justify-start items-center w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded">
+              class="flex justify-start items-center w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded"
+            >
               <font-awesome-icon :icon="faSliders" />
 
               <p class="text-base leading-4">Einstellungen</p>
@@ -65,13 +78,16 @@
 
           <button
             class="flex justify-start items-center w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded"
-            @click="keycloak.keycloak.logout">
+            @click="keycloak.keycloak.logout"
+          >
             <font-awesome-icon :icon="faArrowRightFromBracket" />
 
             <p class="text-base leading-4">Abmelden</p>
           </button>
           <div class="text-white mt-10">
-            <p v-if="keycloakStore.username">{{ keycloakStore.username }} ist eingeloggt</p>
+            <p v-if="keycloakStore.username">
+              {{ keycloakStore.username }} ist eingeloggt
+            </p>
             <p v-else>Not logged in</p>
           </div>
         </div>
@@ -90,7 +106,46 @@
       </footer>
     </div>
     <div v-else>
-      <p>Not authenticated</p>
+      <div
+        id="initial-loader"
+        style="
+          position: fixed;
+          top: 0;
+          left: 0;
+          bottom: 0;
+          right: 0;
+          z-index: 2000;
+          background-color: #65a30d;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        "
+      >
+        <!--  SVG by Sam Herbert (@sherb), for everyone. More @ http://goo.gl/7AJzbL  -->
+        <svg
+          width="38"
+          height="38"
+          viewBox="0 0 38 38"
+          xmlns="http://www.w3.org/2000/svg"
+          stroke="#fff"
+        >
+          <g fill="none" fill-rule="evenodd">
+            <g transform="translate(1 1)" stroke-width="2">
+              <circle stroke-opacity=".5" cx="18" cy="18" r="18"></circle>
+              <path d="M36 18c0-9.94-8.06-18-18-18">
+                <animateTransform
+                  attributeName="transform"
+                  type="rotate"
+                  from="0 18 18"
+                  to="360 18 18"
+                  dur="1s"
+                  repeatCount="indefinite"
+                ></animateTransform>
+              </path>
+            </g>
+          </g>
+        </svg>
+      </div>
     </div>
   </div>
 </template>
@@ -99,7 +154,7 @@
 import keycloak from '@/keycloak/keycloak'
 import { useKeycloakStore } from '@/stores/keycloak'
 import { storeToRefs } from 'pinia'
-import { watch } from 'vue'
+import { computed } from 'vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import {
   faBagShopping,
@@ -108,14 +163,12 @@ import {
   faSplotch,
   faUserGroup,
   faArrowRightFromBracket,
-  faSliders
+  faSliders,
 } from '@fortawesome/free-solid-svg-icons'
 
-const keycloakStore = storeToRefs(useKeycloakStore())
+const keycloakStore = useKeycloakStore()
 
-watch(keycloakStore.isAuthenticated, (newVal) => {
-  return newVal
-})
+const authenticated = computed(() => keycloakStore.authenticated)
 
 const apiUrl = import.meta.env.VITE_API_URL
 </script>
@@ -163,9 +216,8 @@ footer {
 .backoffice-layout {
   tbody {
     tr:hover {
-      background-color: #B3CEB3;
+      background-color: #b3ceb3;
     }
   }
-
 }
 </style>
