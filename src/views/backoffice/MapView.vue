ddff5a44a0fa021fb7ce30794633061f720e8abf
Riddy999 <felix.effenberg@live.com>
2023-11-29 16:30:01 +0100

    implementatioin: leaflet map basic version

--- src/views/backoffice/MapView.vue
+++ src/views/backoffice/MapView.vue
@@ -1,16 +1,30 @@
 <template>
     <component :is="$route.meta.layout || 'div'">
-      <template #header>
-       
-      </template>
+        <template #header>
+            <h1 className="font-bold mt-3 pt-3 text-2xl">{{ $t("menuMap") }}</h1>
+
+        </template>
+        <template #main>
+            <div class="h-full">
+                <div style="height:600px; width:800px">
+                    <l-map ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
+                        <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base"
+                            name="OpenStreetMap"></l-tile-layer>
+                    </l-map>
+                </div>
+            </div>
+        </template>
     </component>
-  </template>
+</template>
   
-  <script lang="ts" setup>
-
-  </script>
+<script lang="ts" setup>
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+const zoom = 2
+</script>
   
-  <style>
-
-  </style>
-  
+<style>
+#map {
+    height: 180px;
+}
+</style>
