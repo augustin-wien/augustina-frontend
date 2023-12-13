85d083aa69e6c28f2f7208664f6f262fea483361
Aaron <aaron@nanu-c.org>
2023-12-13 13:44:50 +0100

    fix: add english translations

--- src/components/EmailModal.vue
+++ src/components/EmailModal.vue
@@ -52,7 +52,7 @@ const validate = () => {
             />
           </svg>
 
-          <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+          <h3 class="mb-5 text-lg font-normal">
             {{
               `${$t('Your item')} ${props.licenceItem.Name} ${$t(
                 'needs an email address.'
@@ -85,7 +85,7 @@ const validate = () => {
               type="button"
               class="text-gray-500 rounded-full bg-white hover:bg-gray-100 mr-3 focus:ring-4 focus:outline-none focus:ring-gray-200 border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
             >
-              {{ $t('No, cancel') }}
+              {{ $t('Back') }}
             </button>
           </RouterLink>
           <button
