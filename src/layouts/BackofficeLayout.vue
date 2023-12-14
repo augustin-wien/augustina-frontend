8f874d61cd8b78249f5bf43d1000e74f3b5d6cb0
Aaron <aaron@nanu-c.org>
2023-12-14 13:04:42 +0100

    fix: set max header width

--- src/layouts/BackofficeLayout.vue
+++ src/layouts/BackofficeLayout.vue
@@ -3,28 +3,37 @@
     <div className="backoffice-layout h-screen flex " v-if="authenticated">
       <div class="h-full flex-none w-100">
         <div
-          className="sidemenu fixed t-0 l-0 h-full w-100 flex-none flex flex-col justify-start items-start pr-5 pl-4 border-gray-600 border-b space-y-3 pb-5 bg-lime-600">
+          className="sidemenu fixed t-0 l-0 h-full w-100 flex-none flex flex-col justify-start items-start pr-5 pl-4 border-gray-600 border-b space-y-3 pb-5 bg-lime-600"
+        >
           <div className="sidemenu-item object-center">
-            <img :src="apiUrl + 'img/logo.png'" alt="Augustin logo" className="logo mx-auto my-5" width="270"
-              height="150" />
+            <img
+              :src="apiUrl + 'img/logo.png'"
+              alt="Augustin logo"
+              className="logo mx-auto my-5"
+              width="270"
+              height="150"
+            />
           </div>
           <RouterLink to="/backoffice/vendorsummary">
             <button
-              class="flex justify-start w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded">
+              class="flex justify-start w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded"
+            >
               <font-awesome-icon :icon="faUserGroup" />
               <p class="text-base leading-4">{{ $t('menuVendors') }}</p>
             </button>
           </RouterLink>
           <RouterLink to="/backoffice/credits" className="sidemenu-link">
             <button
-              class="flex justifyy-start items-center w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded">
+              class="flex justifyy-start items-center w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded"
+            >
               <font-awesome-icon :icon="faBagShopping" />
               <p class="text-base leading-4">{{ $t('menuCredits') }}</p>
             </button>
           </RouterLink>
           <RouterLink to="/backoffice/payouts" className="sidemenu-link">
             <button
-              class="flex justify-start items-center w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded">
+              class="flex justify-start items-center w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded"
+            >
               <font-awesome-icon :icon="faFileLines" />
 
               <p class="text-base leading-4">{{ $t('menuPayouts') }}</p>
@@ -32,7 +41,8 @@
           </RouterLink>
           <RouterLink to="/backoffice/sales" className="sidemenu-link">
             <button
-              class="flex justify-start items-center w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded">
+              class="flex justify-start items-center w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded"
+            >
               <font-awesome-icon :icon="faDungeon" />
 
               <p class="text-base leading-4">{{ $t('menuSales') }}</p>
@@ -40,7 +50,8 @@
           </RouterLink>
           <RouterLink to="/backoffice/payments" className="sidemenu-link">
             <button
-              class="flex justify-start items-center w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded">
+              class="flex justify-start items-center w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded"
+            >
               <font-awesome-icon :icon="faBagShopping" />
 
               <p class="text-base leading-4">{{ $t('menuAccounting') }}</p>
@@ -48,7 +59,8 @@
           </RouterLink>
           <RouterLink to="/backoffice/productsettings" className="sidemenu-link">
             <button
-              class="flex justify-start items-center w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded">
+              class="flex justify-start items-center w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded"
+            >
               <font-awesome-icon :icon="faSplotch" />
 
               <p class="text-base leading-4">{{ $t('menuProducts') }}</p>
@@ -56,7 +68,8 @@
           </RouterLink>
           <RouterLink to="/backoffice/settings" className="sidemenu-link">
             <button
-              class="flex justify-start items-center w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded">
+              class="flex justify-start items-center w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded"
+            >
               <font-awesome-icon :icon="faSliders" />
 
               <p class="text-base leading-4">{{ $t('menuSettings') }}</p>
@@ -64,7 +77,8 @@
           </RouterLink>
           <RouterLink to="/backoffice/map" className="sidemenu-link">
             <button
-              class="flex justify-start items-center w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded">
+              class="flex justify-start items-center w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded"
+            >
               <font-awesome-icon :icon="faMapLocation" />
 
               <p class="text-base leading-4">{{ $t('menuMap') }}</p>
@@ -72,7 +86,8 @@
           </RouterLink>
           <button
             class="flex justify-start items-center w-full space-x-6 focus:outline-none text-white focus:text-indigo-400 pr-5 pb-1 rounded"
-            @click="keycloak.keycloak.logout">
+            @click="keycloak.keycloak.logout"
+          >
             <font-awesome-icon :icon="faArrowRightFromBracket" />
 
             <p class="text-base leading-4">{{ $t('Logout') }}</p>
@@ -85,13 +100,13 @@
           </div>
           <select
             class="h-[40px] w-[70px] bg-lime-700 border-2 border-white font-semibold rounded-full text-white text-center mt-4 mr-4 pl-2 text-sm"
-            v-model="$i18n.locale">
+            v-model="$i18n.locale"
+          >
             <option value="en">EN</option>
             <option value="de">DE</option>
           </select>
         </div>
       </div>
-
 
       <div className="main-container grow h-full flex">
         <div class="header-slot fixed flex-none">
@@ -106,7 +121,9 @@
       </footer>
     </div>
     <div v-else>
-      <div id="initial-loader" style="
+      <div
+        id="initial-loader"
+        style="
           position: fixed;
           top: 0;
           left: 0;
@@ -117,15 +134,28 @@
           display: flex;
           justify-content: center;
           align-items: center;
-        ">
+        "
+      >
         <!--  SVG by Sam Herbert (@sherb), for everyone. More @ http://goo.gl/7AJzbL  -->
-        <svg width="38" height="38" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" stroke="#fff">
+        <svg
+          width="38"
+          height="38"
+          viewBox="0 0 38 38"
+          xmlns="http://www.w3.org/2000/svg"
+          stroke="#fff"
+        >
           <g fill="none" fill-rule="evenodd">
             <g transform="translate(1 1)" stroke-width="2">
               <circle stroke-opacity=".5" cx="18" cy="18" r="18"></circle>
               <path d="M36 18c0-9.94-8.06-18-18-18">
-                <animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="1s"
-                  repeatCount="indefinite"></animateTransform>
+                <animateTransform
+                  attributeName="transform"
+                  type="rotate"
+                  from="0 18 18"
+                  to="360 18 18"
+                  dur="1s"
+                  repeatCount="indefinite"
+                ></animateTransform>
               </path>
             </g>
           </g>
@@ -177,6 +207,7 @@ const apiUrl = import.meta.env.VITE_API_URL
   background: #f2f9f1;
   width: 100%;
   z-index: 50;
+  max-width: calc(100vw - 300px);
 }
 
 .main-slot {
