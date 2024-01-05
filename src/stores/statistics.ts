77f950e2ef32a49919f7f0ffdcd0874ca0fb2bc3
almoxxx <120190420+almoxxx@users.noreply.github.com>
2024-01-05 16:43:04 +0100

    added store

---
+++ src/stores/statistics.ts
@@ -0,0 +1,42 @@
+import { defineStore } from 'pinia'
+import { fetchStatistics } from '@/api/api'
+
+//define interface to store data from backend properly
+export interface Statistics {
+  Items:
+    | [
+        {
+          Id: number
+          Name: string
+          SumAmount: number
+          SumQuantity: number
+        }
+      ]
+    | null
+}
+
+export const useStatisticsStore = defineStore('statistics', {
+  state: () => {
+    return {
+      statistics: [] as Statistics[]
+    }
+  },
+  getters: {
+    statisticsList(state: any) {
+      return state.statistics
+    }
+  },
+
+  actions: {
+    async getPayments(startDate: Date, endDate: Date) {
+      try {
+        const data = await fetchStatistics(startDate, endDate, '')
+        //@ts-ignore
+        this.statistics = data.data
+        //@ts-ignore
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+})
