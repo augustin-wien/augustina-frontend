11b11c2511f53dbefb8cde25faeae4bc9fc5cb25
Riddy999 <felix.effenberg@live.com>
2024-09-09 15:15:26 +0200

    restructured Landing Page

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -5,6 +5,7 @@ import { computed, onMounted } from 'vue'
 import { RouterLink, useRouter } from 'vue-router'
 import { useVendorStore } from '@/stores/vendor'
 import { useShopStore } from '@/stores/ShopStore'
+import IconCross from '@/components/icons/IconCross.vue'
 
 const router = useRouter()
 const shopStore = useShopStore()
@@ -42,38 +43,36 @@ onMounted(() => {
   <component :is="$route.meta.layout || 'div'">
     <template v-if="settStore.settings.MainItemPrice" #main>
       <div className="grid grid-rows-5 h-full place-items-center w-full">
-        <div class="row-span-2 grid grid-rows-3 h-full w-full">
+        <div class="row-span-4 grid grid-rows-5 h-full w-full">
           <div className="text-center font-semibold text-2xl pt-5">
             {{ $t('buyItem') }}
           </div>
           <div class="flex place-content-center">
-            <div class="text-center min-w-fit h-4/5 text-4xl rounded-full text-black font-bold">
+            <div class="text-center min-w-fit h-4/5 text-5xl rounded-full text-black font-bold">
               {{ vendorStore.vendorName }}
             </div>
           </div>
-          <div class="place-items-start w-full flex">
-            
+
+          <RouterLink :to="{ name: 'Shop' }"
+            class="grid grid-rows-2 h-[13vh] w-full customcolor roundedcorner items-center place-items-center">
             <div
-              class="text-xl grow h-[56px] text-center font-semibold text-white bg-black p-3 rounded-full"
-            >
-              1x {{ shopStore.getName(settStore.settings.MainItem) }}
+              class="w-full h-full text-2xl text-center font-semibold text-white bg-black rounded-full place-self-center flex items-center justify-center">
+              <p class="text-center">1x {{ shopStore.getName(settStore.settings.MainItem) }}</p>
             </div>
+            <div className="text-center w-full rounded-full text-2xl font-semibold relative flex justify-center">
+              <div>{{ $t("shop") }}</div>
+              <IconCross class="absolute customfill rotate-45 right-0 place-self-center mr-2"/>
+            </div>
+          </RouterLink>
+          <div className="w-full row-span-2 flex items-center justify-center">
+            <p className="text-center text-8xl font-semibold ">{{ price }}€</p>
           </div>
         </div>
-        <div className="w-full">
-          <p className="text-center text-8xl font-semibold">{{ price }}€</p>
-        </div>
-        <RouterLink :to="{ name: 'Shop'}" class="w-full text-3xl font-semibold place-se">
-          <button className="w-full customcolor p-5 rounded-full text-3xl">
-          {{ $t("shop") }}
-        </button>
-        </RouterLink>
-     
+
+
         <div className="place-items-center w-full flex">
           <RouterLink class="text-center h-[76px] grow" :to="{ name: 'Tippingpage' }">
-            <button
-              class="customcolor background-color rounded-full p-5 text-white text-3xl w-full font-semibold"
-            >
+            <button class="customcolor background-color rounded-full p-5 text-white text-3xl w-full font-semibold">
               {{ $t('next') }}
             </button>
           </RouterLink>
@@ -89,5 +88,13 @@ onMounted(() => {
   background-color: v-bind(settStore.settings.Color);
   color: v-bind(settStore.settings.FontColor);
   fill: v-bind(settStore.settings.FontColor);
+}
+
+.customfill {
+  fill: v-bind(settStore.settings.FontColor);
+}
+
+.roundedcorner {
+  border-radius: calc(13vh / 4);
 }
 </style>
