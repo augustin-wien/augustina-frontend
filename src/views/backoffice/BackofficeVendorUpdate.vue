8e94f2ef20024d8d08981de490ac60693d1f9236
Aaron <aaron@convive.io>
2024-12-05 12:13:34 +0100

    Disable and delete vendors (#177)

--- src/views/backoffice/BackofficeVendorUpdate.vue
+++ src/views/backoffice/BackofficeVendorUpdate.vue
@@ -75,6 +75,42 @@ const updateVendor = async () => {
   }
 }
 
+const deleteVendor = async () => {
+  if (!updatedVendor.value) {
+    return
+  }
+
+  // check if vendor has still a balance
+  if (updatedVendor.value.Balance > 0) {
+    showToast('error', 'VerkäuferIn hat noch Guthaben und kann nicht gelöscht werden')
+    return
+  }
+
+  try {
+    const response = await store.deleteVendor(updatedVendor.value.ID)
+
+    if (response) {
+      // eslint-disable-next-line no-console
+      console.error('Error deleting vendor:', response)
+      showToast('error', 'VerkäuferIn konnte nicht gelöscht werden')
+    } else {
+      showToast('success', 'VerkäuferIn erfolgreich gelöscht')
+      router.push('/backoffice/vendorsummary')
+    }
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Error deleting vendor:', error)
+    showToast('error', 'VerkäuferIn konnte nicht gelöscht werden')
+  }
+}
+
+const showDeleteModal = ref(false)
+
+const showDeleteModalF = (event: Event) => {
+  event.preventDefault()
+  showDeleteModal.value = true
+}
+
 const showToast = (type: string, message: string) => {
   // Set the toast message
   toast.value = { type, message }
@@ -182,8 +218,8 @@ const updateLocation = (newLocation: any) => {
                       class="appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                       required
                     >
-                      <option value="true">{{ $t('yes') }}</option>
-                      <option value="false">{{ $t('no') }}</option>
+                      <option :value="true">{{ $t('yes') }}</option>
+                      <option :value="false">{{ $t('no') }}</option>
                     </select>
                   </div>
                   <label class="block text-gray-700 text-sm font-bold mb-2 pt-3" for="adress"
@@ -383,7 +419,10 @@ const updateLocation = (newLocation: any) => {
                 />
               </div>
             </div>
-            <div class="flex justify-center">
+            <div class="flex justify-between">
+              <button type="submit" class="py-2 px-4 rounded-full" @click="showDeleteModal = true">
+                {{ $t('delete') }}
+              </button>
               <button
                 type="submit"
                 class="py-2 px-4 rounded-full customcolor"
@@ -395,6 +434,79 @@ const updateLocation = (newLocation: any) => {
           </form>
         </div>
         <Toast v-if="toast" :toast="toast" />
+        <!-- delete modal -->
+        <div v-if="showDeleteModal">
+          <div
+            id="defaultModal"
+            tabindex="-1"
+            aria-hidden="false"
+            class="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden flex items-center justify-center overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
+          >
+            <div class="relative w-full max-w-2xl max-h-full">
+              <!-- Modal content -->
+              <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <!-- Modal header -->
+                <div
+                  class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600"
+                >
+                  <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                    {{ updatedVendor.LicenseID }} {{ updatedVendor.FirstName }} {{ $t('delete') }}
+                  </h3>
+                  <button
+                    type="button"
+                    class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                    data-modal-hide="defaultModal"
+                    @click="showDeleteModal = false"
+                  >
+                    <svg
+                      class="w-3 h-3"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 14 14"
+                    >
+                      <path
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                      />
+                    </svg>
+                    <span class="sr-only">Close modal</span>
+                  </button>
+                </div>
+                <!-- Modal body -->
+                <div class="p-6 space-y-6">
+                  <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                    {{ $t('vendordeletionConfirmation') }}
+                  </p>
+                </div>
+                <!-- Modal footer -->
+                <div
+                  class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600"
+                >
+                  <button
+                    data-modal-hide="defaultModal"
+                    type="button"
+                    class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    @click="deleteVendor"
+                  >
+                    {{ $t('delete') }}
+                  </button>
+                  <button
+                    data-modal-hide="defaultModal"
+                    type="button"
+                    class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+                    @click="showDeleteModal = false"
+                  >
+                    {{ $t('cancel') }}
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </template>
   </component>
