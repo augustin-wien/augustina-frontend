ec4b827cec3e92364291ad47b2c53717b0fd69b6
Aaron <aaron@nanu-c.org>
2025-11-04 16:18:22 +0100

    fix shop landing in api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -148,7 +148,7 @@ export async function patchSettings(updatedSettings: Settings) {
   formData.append('QRCodeSettings', updatedSettings.QRCodeSettings)
   formData.append('QRCodeEnableLogo', updatedSettings.QRCodeEnableLogo.toString())
   formData.append('UseTipInsteadOfDonation', updatedSettings.UseTipInsteadOfDonation.toString())
-  formData.append('ShopLanding', updatedSettings.ShopLanding.toString())
+  formData.append('ShopLanding', updatedSettings.ShopLanding ? updatedSettings.ShopLanding.toString() : 'false')
   formData.append('DigitalItemsUrl', updatedSettings.DigitalItemsUrl)
 
   formData.append(
